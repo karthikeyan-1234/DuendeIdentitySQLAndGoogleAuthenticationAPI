56ec52341e91226b210079246960c3618aef0e67
Karthikeyan N <n.karthikeyan@mindsprint.org>
2024-12-27 10:57:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,115 @@
+using ids;
+using ids.Database;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Google;
+
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using Duende.IdentityServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+
+builder.Services.AddRazorPages();
+
+#region Section Start
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString, options => options.MigrationsAssembly(migrationsAssembly));
+});
+
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+#endregion Section End
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+
+    options.EmitStaticAudienceClaim = true;
+    //options.ExternalCookies.AuthenticationScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+})
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(connectionString, opt => opt.MigrationsAssembly(migrationsAssembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(connectionString, opt => opt.MigrationsAssembly(migrationsAssembly));
+    })
+    //.AddTestUsers(Config.Users); //Get rid of in memory store
+    .AddAspNetIdentity<IdentityUser>() //Add this line to use the IdentityUser
+    ;
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+})
+    .AddGoogle(options =>
+    {
+        options.ClientId = configuration["Authentication:Google:ClientId"]!;
+        options.ClientSecret = configuration["Authentication:Google:ClientSecret"]!;
+        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+    });
+
+//Cookie policy
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.Lax; //SameSiteMode.Unspecified;
+    options.Secure = CookieSecurePolicy.None; //CookieSecurePolicy.SameAsRequest;
+    options.CheckConsentNeeded = context => true;
+    options.OnAppendCookie = cookieContext => 
+        CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+    options.OnDeleteCookie = cookieContext =>
+        CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+});
+
+void CheckSameSite(HttpContext context, CookieOptions cookieOptions)
+{
+    if (context.Request.Headers.ContainsKey("X-Forwarded-Proto") &&
+        context.Request.Headers["X-Forwarded-Proto"].FirstOrDefault() == "https")
+    {
+        cookieOptions.SameSite = SameSiteMode.Lax;
+    }
+    else
+    {
+        cookieOptions.SameSite = SameSiteMode.Lax; //SameSiteMode.Strict;
+    }
+}
+
+var app = builder.Build();
+
+app.UseIdentityServer();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages().RequireAuthorization();
+
+if (args.Contains("/seed"))
+{
+    Console.WriteLine("Seeding database...");
+    var config = builder.Configuration;
+    var connectionString_ = config.GetConnectionString("DefaultConnection");
+    SeedData.EnsureSeedData(connectionString_!);
+    SeedData.EnsureUsers(app);
+    Console.WriteLine("Done seeding database.");
+    return;
+}
+
+
+app.Run();
---
+++ SeedData.cs
@@ -0,0 +1,230 @@
+﻿using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Duende.IdentityServer.EntityFramework.Storage;
+
+using IdentityModel;
+
+using ids.Database;
+using ids.Pages;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using System.Security.Claims;
+
+namespace ids
+{
+    public class SeedData
+    {
+        public static void EnsureSeedData(string connectionString)
+        {
+            var services = new ServiceCollection();
+            services.AddOperationalDbContext(options =>
+            {
+                options.ConfigureDbContext = db => db.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName));
+            });
+            services.AddConfigurationDbContext(options =>
+            {
+                options.ConfigureDbContext = db => db.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName));
+            });
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName));
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                scope.ServiceProvider.GetService<PersistedGrantDbContext>()!.Database.Migrate();
+
+                var context = scope.ServiceProvider.GetService<ConfigurationDbContext>();
+                context!.Database.Migrate();
+                EnsureSeedData(context);
+            }
+
+        }
+
+        private static void EnsureSeedData(ConfigurationDbContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                Console.WriteLine("Clients being populated");
+                foreach (var client in Config.Clients.ToList())
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Clients already populated");
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                Console.WriteLine("IdentityResources being populated");
+                foreach (var resource in Config.IdentityResources.ToList())
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("IdentityResources already populated");
+            }
+
+            if (!context.ApiScopes.Any())
+            {
+                Console.WriteLine("ApiScopes being populated");
+                foreach (var resource in Config.ApiScopes.ToList())
+                {
+                    context.ApiScopes.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("ApiScopes already populated");
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                Console.WriteLine("ApiResources being populated");
+                foreach (var resource in Config.ApiResources.ToList())
+                {
+                    context.ApiResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("ApiScopes already populated");
+            }
+
+        }
+
+        public static void EnsureUsers(WebApplication web) //IServiceScope scope
+        {
+            var services = new ServiceCollection();
+            var serviceProvider = services.BuildServiceProvider();
+
+            //Get scope from web
+            using (var scope = web.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                if (userManager == null)
+                {
+                    throw new Exception("Unable to get userManager service resolved for Identity User");
+                }
+
+                #region Create User Alice
+
+                var alice = new IdentityUser
+                {
+                    UserName = "alice",
+                    Email = "",
+                    EmailConfirmed = true
+                };
+
+                if (userManager.FindByNameAsync(alice.UserName).Result == null)
+                {
+                    var result = userManager.CreateAsync(alice, "Pass123$").Result;
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(result.Errors.First().Description);
+                    }
+
+                    result = userManager.AddClaimsAsync(alice, new Claim[]
+                    {
+                    new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                    new Claim(JwtClaimTypes.GivenName, "Alice"),
+                    new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                    new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                    }).Result;
+
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(result.Errors.First().Description);
+                    }
+                }
+
+                #endregion
+
+                #region Create User Bob
+
+                //Create user Bob
+
+                var bob = new IdentityUser
+                {
+                    UserName = "bob",
+                    Email = "",
+                    EmailConfirmed = true
+                };
+
+                if (userManager.FindByNameAsync(bob.UserName).Result == null)
+                {
+                    var result = userManager.CreateAsync(bob, "Pass123$").Result;
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(result.Errors.First().Description);
+                    }
+
+                    result = userManager.AddClaimsAsync(bob, new Claim[]
+                    {
+                    new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                    new Claim(JwtClaimTypes.GivenName, "Bob"),
+                    new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                    new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                    }).Result;
+
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(result.Errors.First().Description);
+                    }
+                }
+                #endregion
+
+                #region Create User Charlie
+
+                //Create user Charlie
+
+                var charlie = new IdentityUser
+                {
+                    UserName = "charlie",
+                    Email = "",
+                    EmailConfirmed = true
+                };
+
+                if (userManager.FindByNameAsync(charlie.UserName).Result == null)
+                {
+                    Console.WriteLine("Creating user Charlie....");
+
+                    var result = userManager.CreateAsync(charlie, "Pass123$").Result;
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(result.Errors.First().Description);
+                    }
+
+                    result = userManager.AddClaimsAsync(charlie, new Claim[]
+                    {
+                    new Claim(JwtClaimTypes.Name, "Charlie Smith"),
+                    new Claim(JwtClaimTypes.GivenName, "Charlie"),
+                    new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                    new Claim(JwtClaimTypes.WebSite, "http://charlie.com"),
+                    }).Result;
+
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(result.Errors.First().Description);
+                    }
+                }
+                #endregion
+            
+            }
+        }
+    }
+}
